bac80ce0ebdedb52e1c506712c5f10ef6510ffe8
ultron01 <p.vurm@outlook.cz>
2023-05-12 07:08:47 +0200

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace HanoiTowers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.animace = new System.Windows.Forms.Timer(this.components);
+            this.lblSouradniceCervena = new System.Windows.Forms.Label();
+            this.lblSouradniceOranzova = new System.Windows.Forms.Label();
+            this.lblSouradniceZelena = new System.Windows.Forms.Label();
+            this.btnPlayAnim = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // animace
+            // 
+            this.animace.Interval = 10;
+            this.animace.Tick += new System.EventHandler(this.animace_Tick);
+            // 
+            // lblSouradniceCervena
+            // 
+            this.lblSouradniceCervena.AutoSize = true;
+            this.lblSouradniceCervena.Location = new System.Drawing.Point(659, 9);
+            this.lblSouradniceCervena.Name = "lblSouradniceCervena";
+            this.lblSouradniceCervena.Size = new System.Drawing.Size(98, 13);
+            this.lblSouradniceCervena.TabIndex = 0;
+            this.lblSouradniceCervena.Text = "Červený blok: [X;Y]";
+            // 
+            // lblSouradniceOranzova
+            // 
+            this.lblSouradniceOranzova.AutoSize = true;
+            this.lblSouradniceOranzova.Location = new System.Drawing.Point(653, 22);
+            this.lblSouradniceOranzova.Name = "lblSouradniceOranzova";
+            this.lblSouradniceOranzova.Size = new System.Drawing.Size(104, 13);
+            this.lblSouradniceOranzova.TabIndex = 1;
+            this.lblSouradniceOranzova.Text = "Oranžový blok: [X;Y]";
+            // 
+            // lblSouradniceZelena
+            // 
+            this.lblSouradniceZelena.AutoSize = true;
+            this.lblSouradniceZelena.Location = new System.Drawing.Point(666, 35);
+            this.lblSouradniceZelena.Name = "lblSouradniceZelena";
+            this.lblSouradniceZelena.Size = new System.Drawing.Size(91, 13);
+            this.lblSouradniceZelena.TabIndex = 2;
+            this.lblSouradniceZelena.Text = "Zelený blok: [X;Y]";
+            // 
+            // btnPlayAnim
+            // 
+            this.btnPlayAnim.Location = new System.Drawing.Point(690, 415);
+            this.btnPlayAnim.Name = "btnPlayAnim";
+            this.btnPlayAnim.Size = new System.Drawing.Size(98, 23);
+            this.btnPlayAnim.TabIndex = 3;
+            this.btnPlayAnim.Text = "Přehraj animaci";
+            this.btnPlayAnim.UseVisualStyleBackColor = true;
+            this.btnPlayAnim.Click += new System.EventHandler(this.btnPlayAnim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPlayAnim);
+            this.Controls.Add(this.lblSouradniceZelena);
+            this.Controls.Add(this.lblSouradniceOranzova);
+            this.Controls.Add(this.lblSouradniceCervena);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Hanoi Towers";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer animace;
+        private System.Windows.Forms.Label lblSouradniceCervena;
+        private System.Windows.Forms.Label lblSouradniceOranzova;
+        private System.Windows.Forms.Label lblSouradniceZelena;
+        private System.Windows.Forms.Button btnPlayAnim;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HanoiTowers
+{
+    public partial class Form1 : Form
+    {
+        public int vez1_red_X1, vez1_red_Y1, vez1_orange_X1, vez1_orange_Y1, vez1_green_X1, vez1_green_Y1;
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            SystemSounds.Question.Play();
+            MessageBox.Show($"Aplikace HanoiTowers\n2023 © Petr Vurm\n\nVytvořeno jako domácí úkol z předmětu PROGRAMOVÁNÍ na SPŠ Hradební.", "O aplikaci", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public bool vez1_nahoru_1, vez1_vlevo_1, vez1_dolu_1, vez1_dolu_zvuk_1, vez1_nahoru_2, vez1_vlevo_2, vez1_dolu_2, vez1_dolu_zvuk_2, vez1_nahoru_3, vez1_vlevo_3, vez1_dolu_3, vez1_dolu_zvuk_3, vez2_nahoru_1, vez2_vlevo_1, vez2_dolu_1, vez2_dolu_zvuk_1, vez2_nahoru_2, vez2_vlevo_2, vez2_dolu_2, vez2_dolu_zvuk_2 = false;
+
+        private void btnPlayAnim_Click(object sender, EventArgs e)
+        {
+            if(animace.Enabled == false)
+            {
+                animace.Enabled = true;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // nastavení dynamických souřadnic X a Y pro červený blok
+            vez1_red_X1 = 50;
+            vez1_red_Y1 = 78;
+
+            // nastavení dynamických souřadnic X a Y pro oranžový blok
+            vez1_orange_X1 = 40;
+            vez1_orange_Y1 = 98;
+
+            // nastavení dynamických souřadnic X a Y pro zelený blok
+            vez1_green_X1 = 30;
+            vez1_green_Y1 = 118;
+        }
+
+        private void animace_Tick(object sender, EventArgs e)
+        {
+            // pohyb červeného bloku nahoru ze základní pozice Y; pouze pokud ještě nebyl pohyb červeného bloku nahoru proveden
+            if (vez1_red_Y1 > 48 && vez1_nahoru_1 == false)
+            {
+                vez1_red_Y1 -= 2;
+                Refresh();
+            }
+
+            // pohyb červeného bloku doleva po dosažení Y = 48 do doby než je dosaženo X = 188; pouze pokud ještě nebyl pohyb červeného bloku  vlevo prováděn
+            if(vez1_red_Y1 == 48 && vez1_red_X1 <= 188 && vez1_vlevo_1 == false)
+            {
+                vez1_red_X1 += 2;
+                vez1_nahoru_1 = true;
+                Refresh();
+            }
+
+            // pohyb červeného bloku dolů po dosažení X = 190 do doby než je dosaženo Y = 116; pouze pokud ještě nebyl pohyb červeného bloku dolů prováděn
+            if(vez1_red_X1 == 190 && vez1_red_Y1 <= 116 && vez1_dolu_1 == false)
+            {
+                vez1_red_Y1 += 2;
+                vez1_vlevo_1 = true;
+                Refresh();
+            }
+
+            // pohyb oranžového bloku nahoru ze základní pozice po dokončení pohybu červeného bloku na X = 190 a Y = 118; pouze pokud ještě nebyl pohyb oranžového bloku nahoru prováděn
+            if(vez1_red_Y1 == 118 && vez1_orange_Y1 > 48 && vez1_nahoru_2 == false)
+            {
+                vez1_orange_Y1 -= 2;
+                vez1_dolu_1 = true;
+                Refresh();
+            }
+
+            // pohyb oranžového bloku doleva po dosažení Y = 48 do doby než je dosaženo X = 178; pouze pokud ještě nebyl pohyb oranžového bloku vlevo prováděn
+            if(vez1_orange_Y1 == 48 && vez1_orange_X1 <= 178 && vez1_vlevo_2 == false)
+            {
+                vez1_orange_X1 += 2;
+                vez1_nahoru_2 = true;
+                Refresh();
+            }
+
+            // pohyb oranžového bloku dolů po dosažení X = 180 do doby než je dosaženo Y = 96; pouze pokud ještě nebyl pohyb oranžového bloku dolů prováděn
+            if (vez1_orange_X1 == 180 && vez1_orange_Y1 <= 96 && vez1_dolu_2 == false)
+            {
+                vez1_orange_Y1 += 2;
+                vez1_vlevo_2 = true;
+                Refresh();
+            }
+
+            // pohyb zeleného bloku nahoru ze základní pozice po dokončení pohybu oranžového bloku na X = 180 a Y = 98; pouze pokud ještě nebyl pohyb zeleného bloku nahoru prováděn
+            if (vez1_orange_Y1 == 98 && vez1_orange_X1 == 180 && vez1_green_Y1 > 48 && vez1_nahoru_3 == false)
+            {
+                vez1_green_Y1 -= 2;
+                vez1_dolu_2 = true;
+                Refresh();
+            }
+
+            // pohyb zeleného bloku doleva po dokončení Y = 48; pouze pokud ještě nebyl pohyb zeleného bloku vlevo prováděn
+            if (vez1_green_Y1 == 48 && vez1_green_X1 <= 308 && vez1_vlevo_3 == false)
+            {
+                vez1_green_X1 += 2;
+                vez1_nahoru_3 = true;
+                Refresh();
+            }
+
+            // pohyb zeleného bloku dolů po dokončení X = 310; pouze pokud ještě nebyl pohyb zeleného bloku dolů prováděn
+            if (vez1_green_X1 == 310 && vez1_green_Y1 <= 116 && vez1_dolu_3 == false)
+            {
+                vez1_green_Y1 += 2;
+                vez1_vlevo_3 = true;
+                Refresh();
+            }
+
+            if(vez1_green_X1 == 310 && vez1_green_Y1 == 118 && vez1_orange_Y1 > 48 && vez2_nahoru_1 == false)
+            {
+                vez1_orange_Y1 -= 2;
+                vez1_dolu_3 = true;
+                Refresh();
+            }
+
+            if(vez1_orange_Y1 == 48 && vez1_orange_X1 <= 318 && vez1_dolu_2 == true && vez2_vlevo_1 == false)
+            {
+                vez1_orange_X1 += 2;
+                vez2_nahoru_1 = true;
+                Refresh();
+            }
+
+            if(vez1_orange_X1 == 320 && vez1_orange_Y1 <= 96 && vez1_nahoru_2 == true && vez2_dolu_1 == false)
+            {
+                vez1_orange_Y1 += 2;
+                vez2_vlevo_1 = true;
+                Refresh();
+            }
+
+            if(vez1_orange_X1 == 320 && vez1_orange_Y1 == 98 && vez1_red_Y1 > 48 && vez2_nahoru_2 == false)
+            {
+                vez1_red_Y1 -= 2;
+                vez2_dolu_1 = true;
+                Refresh();
+            }
+
+            if (vez1_red_Y1 == 48 && vez1_red_X1 <= 328 && vez1_dolu_1 == true && vez2_vlevo_2 == false)
+            {
+                vez1_red_X1 += 2;
+                vez2_nahoru_2 = true;
+                Refresh();
+            }
+
+            if (vez1_red_X1 == 330 && vez1_red_Y1 <= 76 && vez2_dolu_2 == false)
+            {
+                vez1_red_Y1 += 2;
+                vez2_vlevo_2 = true;
+                Refresh();
+            }
+
+            // výpis souřadnic bloku ve tvaru [X; Y]
+            lblSouradniceCervena.Text = $"Červený blok: [{vez1_red_X1};{vez1_red_Y1}]";
+            lblSouradniceOranzova.Text = $"Oranžový blok: [{vez1_orange_X1};{vez1_orange_Y1}]";
+            lblSouradniceZelena.Text = $"Zelený blok: [{vez1_green_X1};{vez1_green_Y1}]";
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics platno = e.Graphics;
+            Pen peroCara = new Pen(Color.Black, 4);
+
+            // věž A
+            platno.DrawLine(peroCara, 20, 140, 120, 140);
+            platno.DrawLine(peroCara, 70, 70, 70, 140);
+
+            // věž B
+            platno.DrawLine(peroCara, 160, 140, 260, 140);
+            platno.DrawLine(peroCara, 210, 70, 210, 140);
+            // platno.FillRectangle(Brushes.Green, 170, 118, 80, 20);
+            // platno.FillRectangle(Brushes.Orange, 180, 98, 60, 20);
+            // platno.FillRectangle(Brushes.Red, 190, 118, 40, 20);
+
+            // věž C
+            platno.DrawLine(peroCara, 300, 140, 400, 140);
+            platno.DrawLine(peroCara, 350, 70, 350, 140);
+            // platno.FillRectangle(Brushes.Green, 310, 118, 80, 20);
+            // platno.FillRectangle(Brushes.Orange, 320, 98, 60, 20);
+            // platno.FillRectangle(Brushes.Red, 330, 78, 40, 20);
+
+            // bloky pro věž A
+            platno.FillRectangle(Brushes.Green, vez1_green_X1, vez1_green_Y1, 80, 20);
+            platno.FillRectangle(Brushes.Orange, vez1_orange_X1, vez1_orange_Y1, 60, 20);
+            platno.FillRectangle(Brushes.Red, vez1_red_X1, vez1_red_Y1, 40, 20);
+
+            if(vez1_red_X1 == 190 && vez1_red_Y1 == 118 && vez1_dolu_zvuk_1 == false)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.block);
+                player.Play();
+                vez1_dolu_zvuk_1 = true;
+            }
+
+            if (vez1_orange_X1 == 180 && vez1_orange_Y1 == 98 && vez1_dolu_zvuk_2 == false)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.block);
+                player.Play();
+                vez1_dolu_zvuk_2 = true;
+            }
+
+            if (vez1_green_X1 == 310 && vez1_green_Y1 == 118 && vez1_dolu_zvuk_3 == false)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.block);
+                player.Play();
+                vez1_dolu_zvuk_3 = true;
+            }
+
+            if (vez1_orange_X1 == 320 && vez1_orange_Y1 == 98 && vez2_dolu_zvuk_1 == false)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.block);
+                player.Play();
+                vez2_dolu_zvuk_1 = true;
+            }
+
+            if (vez1_red_X1 == 330 && vez1_red_Y1 == 78 && vez2_dolu_zvuk_2 == false)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.block);
+                player.Play();
+                vez2_dolu_zvuk_2 = true;
+            }
+        }
+    }
+}
